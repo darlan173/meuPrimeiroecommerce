8a2809c6b62e837230c6f29c888234afdcec4b60
Darlan Correa Alves Ramos <81528373+darlan173@users.noreply.github.com>
2023-05-04 22:35:14 -0300

    primeiro commit

---
+++ src/test/java/br/com/lagoinha/projetoecommerce/ProjetoecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lagoinha.projetoecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
